893be4625e01e6a506164b69a6cbf06fd9062008
Danny Canter <dannycanter123@gmail.com>
2018-08-19 07:10:47 -0400

    Removed redis from settings

--- config/settings.py
+++ config/settings.py
@@ -100,14 +100,20 @@ DATABASES = {
 }
 
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     }
 }
+
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 
 SECRET_KEY = os.environ.get(
     'SECRET_KEY',
